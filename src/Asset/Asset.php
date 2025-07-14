9135383bd79fb512e30a62369c4aa516aea5d06d
Amel Junuzović <maniaba@outlook.com>
2025-07-14 13:57:32 +0200

    Add human-readable size to asset data representation

--- src/Asset/Asset.php
+++ src/Asset/Asset.php
@@ -340,19 +340,20 @@ final class Asset extends Entity implements JsonSerializable
     {
         // need to hide file path on storage
         $data = [
-            'id'                => $this->id,
-            'name'              => $this->name,
-            'file_name'         => $this->file_name,
-            'mime_type'         => $this->mime_type,
-            'size'              => $this->size,
-            'created_at'        => $this->created_at,
-            'updated_at'        => $this->updated_at,
-            'deleted_at'        => $this->deleted_at,
-            'order'             => $this->order,
-            'custom_properties' => $this->getCustomProperties(),
-            'url'               => $this->getUrl(),
-            'url_relative'      => $this->getUrlRelative(),
-            'variants'          => [],
+            'id'                  => $this->id,
+            'name'                => $this->name,
+            'file_name'           => $this->file_name,
+            'mime_type'           => $this->mime_type,
+            'size'                => $this->size,
+            'size_human_readable' => $this->getHumanReadableSize(),
+            'created_at'          => $this->created_at,
+            'updated_at'          => $this->updated_at,
+            'deleted_at'          => $this->deleted_at,
+            'order'               => $this->order,
+            'custom_properties'   => $this->getCustomProperties(),
+            'url'                 => $this->getUrl(),
+            'url_relative'        => $this->getUrlRelative(),
+            'variants'            => [],
         ];
 
         foreach ($this->getMetadata()->assetVariant->getVariants() as $variant) {
