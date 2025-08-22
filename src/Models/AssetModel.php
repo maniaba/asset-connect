c72358ebbe9c107c652fdb49b7b6d56b86e0a889
Amel Junuzović <maniaba@outlook.com>
2025-08-22 10:33:57 +0200

    Add primaryKey property to AssetModel for improved clarity in asset identification

--- src/Models/AssetModel.php
+++ src/Models/AssetModel.php
@@ -19,7 +19,8 @@ use RuntimeException;
  * @method list<Asset>            findAll(int $limit = 0, int $offset = 0)
  * @method Asset|null             first()
  *
- * @property class-string<Asset> $returnType
+ * @property-read string              $primaryKey
+ * @property-read class-string<Asset> $returnType
  */
 class AssetModel extends BaseModel
 {
