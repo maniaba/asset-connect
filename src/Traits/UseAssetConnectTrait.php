810f52257f72df1779e3ac8df10d03a7504f4f6f
Amel Junuzović <maniaba@outlook.com>
2025-09-14 18:32:38 +0200

    Remove abstract constructor requirement from UseAssetConnectTrait for improved flexibility

--- src/Traits/UseAssetConnectTrait.php
+++ src/Traits/UseAssetConnectTrait.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Maniaba\AssetConnect\Traits;
 
-use CodeIgniter\Entity\Entity;
 use CodeIgniter\Files\File;
 use CodeIgniter\HTTP\Files\UploadedFile;
 use Config\Services;
