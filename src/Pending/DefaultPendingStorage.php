1a7a093f2b873d10973e3ba07e0d6767e2a527ff
Amel Junuzović <maniaba@outlook.com>
2025-12-03 08:33:56 +0100

    Prevent unnecessary deletion of temporary files in DefaultPendingStorage

--- src/Pending/DefaultPendingStorage.php
+++ src/Pending/DefaultPendingStorage.php
@@ -175,8 +175,10 @@ class DefaultPendingStorage implements PendingStorageInterface
 
         // if file already exists at path, return because we do not want to overwrite
         if (file_exists($storeFilePath)) {
-            // Delete the temporary file if it was created (since we're not using it for update)
-            @unlink($asset->file->getRealPath());
+            if ($storeFilePath !== $asset->file->getRealPath()) {
+                // Delete the temporary file if it was created (since we're not using it for update)
+                @unlink($asset->file->getRealPath());
+            }
 
             return;
         }
